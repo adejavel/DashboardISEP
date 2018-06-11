9bce41a96723414d62527729eb1d98060c21e697
Aymeric de Javel <aymeric@dejavel.fr>
2018-06-11 19:05:36 +0200

    changing api

--- back/src/main/java/com/rhododendron/dashboardisep/controlers/TaskControler.java
+++ back/src/main/java/com/rhododendron/dashboardisep/controlers/TaskControler.java
@@ -141,7 +141,7 @@ public class TaskControler {
 
     }
     @CrossOrigin(origins = "*")
-    @PostMapping(path="/modify/{id}")
+    @PutMapping(path="/modify/{id}")
     public @ResponseBody Task changeTask(@RequestBody Task task,@PathVariable(value = "id") String id) {
         try {
             Task original = taskRepository.findById(Long.valueOf(Integer.parseInt(id))).get();
